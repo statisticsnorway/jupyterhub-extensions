f055a067587878aa881873d728bf588776e1fa4c
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2021-05-19 14:56:54 +0200

    Bump version: 0.0.6 → 0.0.7

--- TokenExchangeAuthenticator/setup.py
+++ TokenExchangeAuthenticator/setup.py
@@ -21,7 +21,7 @@ DEPENDENCIES = [
 
 setup(
     name="TokenExchangeAuthenticator",
-    version="0.0.6",
+    version="0.0.7",
     url="https://github.com/statisticsnorway/jupyterhub-extensions",
     license='MIT',
 
