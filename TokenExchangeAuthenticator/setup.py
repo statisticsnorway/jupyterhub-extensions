5a207844d323ca8f3013a6a1dfa61c1f7c65b869
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2022-10-04 12:21:10 +0200

    Upgrade dependencies (#5)
    * Upgrade jupyterhub, oauthenticator and pyjwt dependencies

--- TokenExchangeAuthenticator/setup.py
+++ TokenExchangeAuthenticator/setup.py
@@ -14,9 +14,9 @@ def read(filename):
 
 
 DEPENDENCIES = [
-    'jupyterhub>=1.4.2',
-    'oauthenticator>=14.1.0',
-    'pyjwt[crypto]>=1.7.1'
+    'jupyterhub>=3.0.0',
+    'oauthenticator>=15.1.0',
+    'pyjwt[crypto]>=2.4.0'
 ]
 
 setup(
